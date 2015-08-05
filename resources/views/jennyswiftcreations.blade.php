750c4219526f57d5b7b45a8710b19e6e63c29020
Jenny Swift <cheezyspaghetti@gmail.com>
2015-08-05 20:08:02 +1000

    Work on header and footer

--- resources/views/jennyswiftcreations.blade.php
+++ resources/views/jennyswiftcreations.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>jennyswiftcreations</title>
+    <link rel="stylesheet" href="tools/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<style>
 		p {
@@ -11,6 +12,7 @@
 	</style>
 </head>
 <body>
+    @include('templates.header')
 
 	<div class="main">
 		<p>Hi! There's nothing to see here yet, but I am working on a couple of web applications so hopefully they will be here to check out one day! :)</p>
@@ -18,7 +20,13 @@
 	</div>
 	
 	<footer>
-		<a href="<?php echo route('privacy-policy'); ?>">Privacy Policy</a>
+        <li>
+            <a href="<?php echo route('privacy-policy'); ?>">Privacy Policy</a>
+        </li>
+
+        <li>
+            <a href="#">Credits</a>
+        </li>
 	</footer>
 		
 </body>
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,12 @@
+
+<ul id="navbar" style="z-index:1000">
+
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Creations</a>
+        </li>
+
+
+</ul>
