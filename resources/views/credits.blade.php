9982fc2aa728f2b2073948195ea9ce6c33f8f660
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-20 16:16:28 +1000

    Credits page

--- resources/views/credits.blade.php
+++ resources/views/credits.blade.php
@@ -12,25 +12,38 @@
 
 <div class="main" id="credits">
     <h1>Credits</h1>
+    
+    <p><a href="https://teamtreehouse.com">Team Treehouse</a>. My first experience of learning to code was with their free trial. The term 'coding' was still new to me and if it wasn't for them I may not have begun the wonderful journey of coding.</p>
+
+    <p><a href="https://www.codeschool.com/">Code School</a> and <a href="https://laracasts.com/">Laracasts</a>, where I have also learned to code.</p>
+
+    <p>The generous people who contribute on <a href="http://stackoverflow.com/">stackoverflow</a>, the forums at <a href="https://laracasts.com/">Laracasts</a> and <a href="https://teamtreehouse.com">Team Treehouse</a>, and who share their work on <a href="https://github.com/">GitHub</a>.</p>
+
+    <p>My brother Pete, for thinking up a real project for me to make, for his patience, and brainstorming.</p>
+
+    <p>My mum, for getting excited when she sees what I can create, and for brainstorming.</p>
+
+    <p><a href="https://www.codementor.io/foxted">Val</a>, my mentor, for changing my life. You can read my reviews for him <a
+                href="https://www.codementor.io/foxted#/review">here</a>.</p>
+
+    <p><a href="https://www.codementor.io/nihantanu">Nish</a>, for the support and friendship.</p>
+
+    <p>The following libraries/frameworks:</p>
+    <ul>
+        <li><a href="http://laravel.com/">Laravel</a></li>
+        <li><a href="https://angularjs.org/">AngularJS</a></li>
+        <li><a href="http://jquery.com/">jQuery</a></li>
+        <li><a href="http://getbootstrap.com/">Twitter Bootstrap</a></li>
+        <li><a href="http://datejs.com/">Datejs</a></li>
+        <li><a href="http://momentjs.com/">Moment.js</a></li>
+        <li><a href="http://fontawesome.io">Font Awesome by Dave Gandy</a></li>
+    </ul>
+
+    <p>God, for bringing the joy of coding into my life.</p>
+
+    {{--<p><a href="http://glyphicons.com/">Glyphicons</a>, thank you for making your glyphs available for Bootstrap.</p>--}}
 
 </div>
-{{--<p><a href="http://getbootstrap.com/">Bootstrap</a></p>--}}
-
-{{--<p><a href="http://datejs.com/">Datejs</a></p>--}}
-
-{{--<p><a href="http://fontawesome.io">Font Awesome by Dave Gandy</a></p>--}}
-
-{{--<p><a href="http://momentjs.com/">Moment.js</a></p>--}}
-
-{{--<p><a href="https://teamtreehouse.com">Treehouse</a>, thank you for teaching me how to code.</p>--}}
-
-{{--<p><a href="https://www.codeschool.com/">Code School</a>, you have also helped me learn to code.</p>--}}
-
-{{--<p><a href="http://getbootstrap.com/">Bootstrap</a>, what a delight it was when I realized what I could do with you.</p>--}}
-
-{{--<p><a href="http://glyphicons.com/">Glyphicons</a>, thank you for making your glyphs available for Bootstrap.</p>--}}
-
-{{--<p>Thank you to those who contribute to <a href="http://stackoverflow.com/">stackoverflow</a>. The answers on the forum have been a great help to me.</p>--}}
 
 @include('templates.footer')
 
