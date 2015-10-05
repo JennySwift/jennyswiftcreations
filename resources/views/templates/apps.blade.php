a01143300a544fa9480a62b62b650ee781f2dc31
Jenny Swift <cheezyspaghetti@gmail.com>
2015-10-05 21:50:39 +1100

    Add reviews and clean sass

---
+++ resources/views/templates/apps.blade.php
@@ -0,0 +1,41 @@
+<div class="multi-app-container">
+
+    <div class="app-container">
+        <h1>Budget App</h1>
+
+        <div class="info-container">
+            <p>This is my main app. Originally made for my brother, with the goal of providing a useful way to budget when income is unpredictable.</p>
+        </div>
+
+        <div class="btn-container">
+            {{--<button class="btn btn-warning">Info</button>--}}
+            <a class="btn btn-success" href="http://budget_playground.jennyswiftcreations.com/">Demo</a>
+        </div>
+
+        <div class="img-container">
+            <img src="img/budget-app.png" alt=""/>
+        </div>
+
+
+    </div>
+
+    <div class="app-container">
+        <h1>Dental App</h1>
+
+        <div class="info-container">
+            <p>A small app to keep track of patient data. Made for my mum who uses it at work on a daily basis.</p>
+        </div>
+
+        <div class="btn-container">
+            <a class="btn btn-success" href="http://dental_playground.jennyswiftcreations.com/">Demo</a>
+        </div>
+
+        <div class="img-container">
+            <img src="img/dental-app.png" alt=""/>
+        </div>
+
+
+
+    </div>
+
+</div>
---
+++ resources/views/templates/reviews.blade.php
@@ -0,0 +1,46 @@
+<h1>Reviews</h1>
+<div id="reviews">
+
+    <div class="review">
+        <p>To whom it may concern,</p>
+
+        <p>I am privileged to write in support of my dear friend and mentee, Jenny Swift. I have been mentoring Jenny since
+            March 2015, remotely via Codementor.io, specifically on PHP and the Laravel framework.</p>
+
+        <p>We began with small sessions in order to help Jenny in the debugging process of her applications. Jenny was
+            working on her own projects using Laravel and Angular.js after only a few months of learning code and was
+            definitely motivated to improve her programming skills.</p>
+
+        <p>After a few sessions, Jenny told me about her ultimate goal to build a career as a web developer. We then
+            decided to start working together on theory of Object Oriented Programming in PHP, design patterns and advanced
+            use of Laravel including Test-Driven Development and RESTful API specifications. Her learning speed and
+            dedication to improve are astounding. She is always working hard to apply the new concepts learned during our
+            sessions into her applications, by refactoring existing features or implementing new ones.</p>
+
+        <p>Not only does Jenny demonstrate commitment and motivation into becoming a web developer, but she also possesses
+            rare personal skills with her calmness and empathy, and her practical and realistic reasoning — not to mention
+            her positive energy.</p>
+
+        <p>Our sessions being remote, her autonomy, great written communication skills, proficiency with version control
+            and team organization tools (eg. Git, Trello, Skype, etc.) are definitely appreciated.</p>
+
+        <p>I would strongly recommend Jenny for an entry level or junior position as a PHP developer.</p>
+
+        <p>Sincerely,</p>
+
+        <p>Valentin Prugnaud
+            <br>http://codementor.io/foxted
+            <br>valentin@foxted.com</p>
+    </div>
+
+    <div class="review">
+        <p>I was mentor of Jenny and worked with her almost 6 month.Jenny is a quick learner and she has very deep understanding of Laravel and UI stack framework. Along with her strong technical understanding of how things works, she always eager to learn new technology and learn things by doing. She truly cares about helping other, very patient and have great team spirit. I would highly recommend to jenny to be on team If you are looking for full stack developer.</p>
+
+        <p>Nishant Kumar
+            <br>https://www.codementor.io/nihantanu
+        </p>
+    </div>
+
+
+
+</div>
