8b03c10b8cfef8bf448d7471f8e77198fbf1c2e1
Jenny Swift <cheezyspaghetti@gmail.com>
2016-05-21 18:37:55 +1000

    Add lists app to portfolio

--- resources/views/templates/apps.blade.php
+++ resources/views/templates/apps.blade.php
@@ -4,7 +4,7 @@
         <h1>Budget App</h1>
 
         <div class="info-container">
-            <p>This is my main app. Originally made for my brother, with the goal of providing a useful way to budget when income is unpredictable.</p>
+            <p>This was my first main app. Originally made for my brother, with the goal of providing a useful way to budget when income is unpredictable.</p>
         </div>
 
         <div class="btn-container">
@@ -14,6 +14,22 @@
 
         <div class="img-container">
             <img src="img/budget-app.png" alt=""/>
+        </div>
+    </div>
+
+    <div class="app-container">
+        <h1>Lists App</h1>
+
+        <div class="info-container">
+            <p>A tree-style app for managing and prioritizing tasks. I use it to build my apps. :)</p>
+        </div>
+
+        <div class="btn-container">
+            <a class="btn btn-success" href="http://listsplayground.jennyswiftcreations.com">Demo</a>
+        </div>
+
+        <div class="img-container">
+            <img src="img/lists.png" alt=""/>
         </div>
     </div>
 
