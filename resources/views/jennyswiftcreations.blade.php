245f279863f315d2c386112ff869ccd43fd50405
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-22 15:35:17 +1000

    Styling

--- resources/views/jennyswiftcreations.blade.php
+++ resources/views/jennyswiftcreations.blade.php
@@ -5,14 +5,15 @@
 	<title>jennyswiftcreations</title>
     <link rel="stylesheet" href="tools/bootstrap.min.css">
     <link rel="stylesheet" href="tools/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     @include('templates.header')
 
 	<div class="main">
 
-        <h3>Hi and thanks for visiting! :) This site and my apps are still in progress, but feel free to have a look around and try out my demos!</h3>
+        <p class="header">Hi and thanks for visiting! :) This site and my apps are still in progress, but feel free to have a look around and try out my demos!</p>
 
         {{--<div class="looking-for-work">--}}
             {{--<p class="alert alert-success">I am looking for remote work. If you are interested, I'd love to hear from you. :)</p>--}}
