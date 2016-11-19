eca2bb1cef6f49b9ddba1ad13b845d5784085044
Jenny Swift <cheezyspaghetti@gmail.com>
2016-11-19 15:18:00 +1100

    Clean contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,15 +12,15 @@
 
         <div class="content">
             <div>
-                <span>Email: <a href="mailto:jennyswiftcreations@gmail.com">jennyswiftcreations<i class="fa fa-at"></i>gmail.com</a></span>
+                <span><a href="mailto:jennyswiftcreations@gmail.com">Email</a></span>
             </div>
 
             <div>
-                <span>Github: <a href="https://github.com/JennySwift">https://github.com/JennySwift</a></span>
+                <span><a href="https://github.com/JennySwift">Github</a></span>
             </div>
 
             <div>
-                <span>LinkedIn: <a href="https://au.linkedin.com/pub/jenny-swift/b5/446/576">https://au.linkedin.com/pub/jenny-swift/b5/446/576</a></span>
+                <span><a href="https://au.linkedin.com/pub/jenny-swift/b5/446/576">LinkedIn</a></span>
             </div>
         </div>
 
