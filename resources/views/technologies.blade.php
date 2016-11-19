e0a7764ea8060eef975953aad66409960ba1e0ac
Jenny Swift <cheezyspaghetti@gmail.com>
2016-11-19 15:36:28 +1100

    Add technologies

--- resources/views/technologies.blade.php
+++ resources/views/technologies.blade.php
@@ -11,6 +11,42 @@
 <div class="main">
 
 	<h1>Technologies</h1>
+	
+	<ul>
+		<li>HTML5</li>
+		<li>CSS3</li>
+		<li>Sass</li>
+		<li>JavaScript</li>
+		<li>PHP</li>
+
+		<li>MySQL</li>
+
+		<li>Laravel</li>
+		<li>Vue.js</li>
+		<li>AngularJS</li>
+		<li>Twitter Bootstrap</li>
+
+		<li>jQuery</li>
+
+		<li>PhpStorm</li>
+		<li>Sublime Text</li>
+
+		<li>Laravel Homestead</li>
+		<li>Laravel Forge</li>
+
+		<li>Webpack</li>
+		<li>Vagrant</li>
+		<li>Karma</li>
+		<li>Composer</li>
+		<li>npm</li>
+		<li>Git</li>
+
+		<li>Pusher</li>
+
+		<li>Mocha</li>
+		<li>Chai</li>
+		<li>PHPUnit</li>
+	</ul>
 
 </div>
 
