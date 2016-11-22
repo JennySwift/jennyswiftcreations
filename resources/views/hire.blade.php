52791892ea578b4d6a6520f4f767d838cd3556eb
Jenny Swift <cheezyspaghetti@gmail.com>
2016-11-23 08:48:38 +1100

    Hire page

--- resources/views/hire.blade.php
+++ resources/views/hire.blade.php
@@ -12,6 +12,17 @@
 
         <h1>Hire</h1>
 
+		<p>I’m a junior full-stack web developer. I love working remotely. I’m not quite sure why more companies don’t
+			allow it, when pretty much all I need is my computer and an internet connection. Perhaps my cleaner friend
+			put it better: I “could work while sitting on the toilet."<img src="emoticons/happy-laugh.png" class="emoticon"></p>
+
+
+
+		<p>So if you have work I can do on the toilet...kidding. ;)</p>
+		<p>If you have remote work for me, I’d love to hear from you.</p>
+
+		<p>But if you have work for me that can’t be done remotely, I’d still like to hear from you.</p>
+
 
 	</div>
 	
