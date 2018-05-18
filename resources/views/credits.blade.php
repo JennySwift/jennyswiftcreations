ace50b2ef1ef1e1f6fd64b109aa3fd7f5c2991ca
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-18 10:31:23 +1000

    Update credits page

--- resources/views/credits.blade.php
+++ resources/views/credits.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div id="credits">
-        <p><a href="https://teamtreehouse.com">Team Treehouse</a>. My first experience of learning to code was with their free trial. The term 'coding' was still new to me and if it wasn't for them I may not have begun the wonderful journey of coding.</p>
+        <p><a href="https://teamtreehouse.com">Team Treehouse</a>. My first experience of learning to code was with their free trial. The term 'coding' was still new to me, and if it wasn't for them, I may not have begun the wonderful journey of coding.</p>
 
         <p><a href="https://www.codeschool.com/">Code School</a> and <a href="https://laracasts.com/">Laracasts</a>, where I have also learned to code.</p>
 
-        <p>The generous people who contribute on <a href="http://stackoverflow.com/">stackoverflow</a>, the forums at <a href="https://laracasts.com/">Laracasts</a> and <a href="https://teamtreehouse.com">Team Treehouse</a>, and who share their work on <a href="https://github.com/">GitHub</a>.</p>
+        <p>The generous people who contribute on forums such as <a href="http://stackoverflow.com/">stackoverflow</a>, <a href="https://laracasts.com/">Laracasts</a>, and <a href="https://teamtreehouse.com">Team Treehouse</a>, and who share their work on <a href="https://github.com/">GitHub</a>.</p>
 
-        <p>My brother Pete, for thinking up a real project for me to make, for his patience, and brainstorming.</p>
+        <p>My brother, Pete, for giving me projects to work on, and for brainstorming with me.</p>
 
-        <p>My mum, for getting excited when she sees what I can create, and for brainstorming.</p>
+        <p>My mum, for getting excited about what I make.</p>
 
         <p>
             <a href="https://www.codementor.io/foxted">Val</a>,
@@ -21,28 +21,22 @@
 
         <p><a href="https://www.codementor.io/">Codementor</a>, for providing the circumstances for my life to be changed.</p>
 
-        <p><a href="https://www.codementor.io/nihantanu">Nish</a>, for the support and friendship.</p>
-
         <p>The following technologies/services:</p>
         <ul>
             <li><a href="http://laravel.com/">Laravel</a></li>
             <li><a href="http://vuejs.org/">Vue.js</a></li>
+            <li><a href="https://framework7.io/">Framework7</a></li>
             <li><a href="https://git-scm.com/">Git</a></li>
             <li><a href="https://www.npmjs.com/">npm</a></li>
             <li><a href="http://sass-lang.com/">Sass</a></li>
             <li><a href="http://getbootstrap.com/">Twitter Bootstrap</a></li>
-            <li><a href="http://underscorejs.org/">Underscore.js</a></li>
             <li><a href="https://lodash.com/">Lodash</a></li>
-            <li><a href="http://jquery.com/">jQuery</a></li>
             <li><a href="https://daneden.github.io/animate.css/">Animate.css</a></li>
             <li><a href="https://sugarjs.com/">Sugar</a></li>
-            <li><a href="http://momentjs.com/">Moment.js</a></li>
             <li><a href="http://fontawesome.io">Font Awesome by Dave Gandy</a></li>
             <li><a href="http://www.smiley.com/emo">http://www.smiley.com/emo</a></li>
             <li>And so many more...</li>
         </ul>
-
-        <p>God, for bringing the joy of coding into my life.</p>
 
         {{--<p><a href="http://glyphicons.com/">Glyphicons</a>, thank you for making your glyphs available for Bootstrap.</p>--}}
 
